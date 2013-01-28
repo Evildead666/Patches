62429599160bc54cea75e891d5606f858c176daa
Evildead666 <Master_R_U@gmx.de>
2013-01-28 22:07:08 +0100

    second commit

---
+++ Q006543S_280113_The Warsong Reports.sql
@@ -0,0 +1,13 @@
+#Q006543S_280113_The Warsong Reports
+#Correcting Zone for this quest to Ashenvale http://www.wowhead.com/quest=6543
+#Quest ID 6543
+#Old Values
+#UPDATE quest_template SET ZoneOrSort=17 WHERE entry=6543;
+
+
+
+UPDATE quest_template SET ZoneOrSort=331 WHERE entry=6543;
+
+
+
+#END OF PATCH
